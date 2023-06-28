1bffa492b7a0136c6a42bc1f752d6f6de9fe2e8a
erwinjunior <nguyendinhbao0770@gmail.com>
2023-06-28 17:05:43 +0700

    render product list

---
+++ data.js
@@ -0,0 +1,189 @@
+export const categories = [
+    {
+        id: 1,
+        image: './assets/img/banh-keo.jpg',
+        name: 'Kẹo & me các loại',
+    },
+    {
+        id: 2,
+        image: './assets/img/banh-cac-loai.jpg',
+        name: 'Bánh các loại',
+    },
+    {
+        id: 3,
+        image: './assets/img/hang-nhap-khau.jpg',
+        name: 'Hàng nhập các loại',
+    },
+    {
+        id: 4,
+        image: './assets/img/do-an-vat.jpg',
+        name: 'Đồ ăn vặt',
+    },
+    {
+        id: 5,
+        image: './assets/img/hat-dinh-duong.jpg',
+        name: 'Hạt dinh dưỡng',
+    },
+    {
+        id: 6,
+        image: './assets/img/trai-cay-say-kho.jpg',
+        name: 'Trái cây sấy khô',
+    },
+    {
+        id: 7,
+        image: './assets/img/kho-ca-loai.jpg',
+        name: 'Khô tẩm vị',
+    },
+    {
+        id: 8,
+        image: './assets/img/cac-loai-dau.jpg',
+        name: 'Đậu các loại',
+    },
+    {
+        id: 9,
+        image: './assets/img/mut-cac-loai.jpg',
+        name: 'Mứt các loại',
+    },
+    {
+        id: 10,
+        image: './assets/img/trai-cay-say.jpg',
+        name: 'Trái Cây sấy dẻo',
+    },
+];
+
+export const products = [
+    {
+        id: 0,
+        image: './assets/img/banh-sua-chua-ong-gia.jpg',
+        name: 'Bánh sữa chua ông già',
+        info: [{ cost: '300.000d', unit: '2kg' }],
+    },
+    {
+        id: 1,
+        name: 'Miến Trùng Khánh',
+        image: './assets/img/mien-trung-khanh.jpg',
+        info: [{ cost: '57.000d', unit: 'thùng' }],
+    },
+    {
+        id: 2,
+        name: 'Kẹo sữa chua ông già',
+        image: './assets/img/keo-sua-chua-ong-gia.jpg',
+        info: [{ cost: '42.000d', unit: '360g' }],
+    },
+    {
+        id: 3,
+        name: 'Omai Cherry Việt Quất',
+        image: './assets/img/omai-jerry-vietquat.jpg',
+        info: [{ cost: '33.000d', unit: '408g' }],
+    },
+    {
+        id: 4,
+        name: 'Thạch Jopy',
+        image: './assets/img/thach-jopy.jpg',
+        info: [{ cost: '48.000d', unit: '770g' }],
+    },
+    {
+        id: 5,
+        name: 'Bột cacao Đắk Lắk',
+        image: './assets/img/bot-ca-cao-daklak.jpeg',
+        info: [{ cost: '41.000d', unit: '500g' }],
+    },
+    {
+        id: 6,
+        name: 'Kẹo Sữa Bò Viên Nén',
+        image: './assets/img/keo-sua-bo-vien-nen.jpg',
+        info: [{ cost: '35.000d', unit: '250g' }],
+    },
+    {
+        id: 7,
+        name: 'Omai Việt Quất Plums',
+        image: './assets/img/omai-viet-quat.jpg',
+        info: [{ cost: '33.000d', unit: '408g' }],
+    },
+    {
+        id: 8,
+        name: 'Mì Vàng Enaak',
+        image: './assets/img/mi-vang-ennak.jpg',
+        info: [{ cost: '109.000d', unit: 'thùng' }],
+    },
+    {
+        id: 9,
+        name: 'Muối Tôm Như Ý - Hạt Lớn',
+        image: './assets/img/muoi-tom-nhu-y-hat-lon.jpg',
+        info: [{ cost: '70.000d', unit: '400g' }],
+    },
+    {
+        id: 10,
+        name: 'Nho Raisin Sunview Lon',
+        image: './assets/img/nho-raisin-sun-view-Lon.jpg',
+        info: [
+            { cost: '55.000d', unit: '425g' },
+            { cost: '630.000d', unit: 'thùng 12h' },
+        ],
+    },
+    {
+        id: 11,
+        name: 'Hạt Hướng Dương Bà Già',
+        image: './assets/img/banh-sua-chua-ong-gia.jpg',
+        info: [{ cost: '47.000d', unit: '500g' }],
+    },
+    {
+        id: 12,
+        name: 'Táo đỏ Hàn Quốc',
+        image: './assets/img/tao-do-hanquoc.jpg',
+        info: [{ cost: '60.000d', unit: '1kg' }],
+    },
+    {
+        id: 13,
+        name: 'Omai Cherry Plums',
+        image: './assets/img/omai-cherry-plus.jpg',
+        info: [{ cost: '33.000d', unit: '408g' }],
+    },
+    {
+        id: 14,
+        name: 'Bánh Ruốc Gấu',
+        image: './assets/img/banh-ruoc-gau.jpg',
+        info: [{ cost: '108.000d', unit: '1.5kg' }],
+    },
+    {
+        id: 15,
+        name: 'Hạt chia đen Úc',
+        image: './assets/img/hat-chia-den-uc.jpg',
+        info: [{ cost: '56.000d', unit: '500g' }],
+    },
+    {
+        id: 16,
+        name: 'Muối tôm Như Ý - Lốc 60g',
+        image: './assets/img/muoi-tom-nhu-y-loc60g.jpg',
+        info: [{ cost: '62.000d', unit: '720g' }],
+    },
+    {
+        id: 17,
+        name: 'Kẹo Dẻo Trái Cây Top Fruit',
+        image: './assets/img/keo-deo-trai-cay.jpg',
+        info: [{ cost: '34.000d', unit: '320g' }],
+    },
+    {
+        id: 18,
+        name: 'Choco Cube Thanh',
+        image: './assets/img/banh-sua-chua-ong-gia.jpg',
+        info: [{ cost: '58.000d', unit: '687g' }],
+    },
+    {
+        id: 19,
+        name: 'Bắp Chai Mini',
+        image: './assets/img/bap-chai-mini.jpg',
+        info: [{ cost: '38.000d', unit: '200g' }],
+    },
+    {
+        id: 20,
+        name: 'Chè Dưỡng Nhan',
+        image: './assets/img/che-duong-nhan.jpg',
+        info: [{ cost: '32.000d', unit: '350g' }],
+    },
+    // {
+    //     id: 0,
+    //     image: './assets/img/banh-sua-chua-ong-gia.jpg',
+    //     info: [{ cost: '300.000d', unit: '2kg' }],
+    // },
+];
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import products from './products.js';
+import { categories, products } from './data.js';
 
-// render products
+// render categories
 
-const productList = document.querySelector('.product__list');
+const categoryList = document.querySelector('.product__list');
 
-const productsHtml = products.map(
-    (product) => `<div class="product__item">
+const categoriesHtml = categories.map(
+    (category) => `<div class="product__item">
             <div class="product__image">
-                <img src="${product.image}" />
+                <img src="${category.image}" />
             </div>
-            <h4 class="product__name">${product.name}</h4>
+            <h4 class="product__name">${category.name}</h4>
         </div>`
 );
 
-productList.innerHTML = productsHtml.join('');
+categoryList.innerHTML = categoriesHtml.join('');
 
 // handle header display fixed
 
@@ -25,3 +25,33 @@ document.addEventListener('scroll', () => {
     if (window.pageYOffset >= HALF_HEADER_HEIGHT) header.classList.add(HEADER__FIXED_CLASS);
     else header.classList.remove(HEADER__FIXED_CLASS);
 });
+
+// render products
+const productList = document.querySelector('tbody');
+const productsHtml = products.map(
+    (product) =>
+        `<tr>
+            <td class="product__img">
+                <div>
+                    <img
+                        src="${product.image}"
+                        alt=""
+                    />
+                </div>
+            </td>
+            <td class="product__info">
+                <strong class="product__detail-name">
+                    ${product.name}
+                </strong>
+                ${product.info.map(
+                    (item) => `<div>
+                <input type="checkbox" />
+                     <span class="product__">${item.cost} - ${item.unit}</span>
+            </div>`
+                )}
+                
+            </td>
+        </tr>`
+);
+
+productList.innerHTML = productsHtml.join('');
